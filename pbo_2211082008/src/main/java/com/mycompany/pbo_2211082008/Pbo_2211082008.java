e17f2f80e2430557cd3c639ef6e452373b010fea
Farhaan4 <127176994+Farhaan4@users.noreply.github.com>
2023-04-05 11:35:06 +0700

    LATIHAN

---
+++ pbo_2211082008/src/main/java/com/mycompany/pbo_2211082008/Pbo_2211082008.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_2211082008;
+
+/**
+ *
+ * @author USER
+ */
+public class Pbo_2211082008 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
